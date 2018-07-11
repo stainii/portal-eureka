9d1805eb1212fe5a029a15156c30ebfde1bb0934
stijnhooft@hotmail.com <stijnhoof@hotmail.com>
2018-07-11 16:52:34 +0200

    Rename startup class to be consistent with other modules

--- src/main/java/be/stijnhooft/portal/eureka/PortalEureka.java
+++ src/main/java/be/stijnhooft/portal/eureka/PortalEureka.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaApplication {
+public class PortalEureka {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(PortalEureka.class, args);
 	}
 }
